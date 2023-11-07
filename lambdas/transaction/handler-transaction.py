ce53b134887d903030aeb7d81b27729eb1cccca0
joelSuarez <joel.suarez@linktic.co>
2023-11-07 09:36:25 -0500

    fixes

--- lambdas/transaction/handler-transaction.py
+++ lambdas/transaction/handler-transaction.py
@@ -9,59 +9,9 @@ import uuid
 import json
 
 
-# import jwt
-# from jwt import PyJWKClient
-# import httpx
-
-
-
 STAGE = os.environ.get('STAGE')
 root_path = '/' if not STAGE else f'/{STAGE}'
 
-# COGNITO_REGION = os.environ.get("AWS_REGION")
-# COGNITO_USER_POOL_ID =  os.environ.get("user_pool_id")
-
-# async def get_cognito_jwt_secret() -> str:
-#     JWKS_URL = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
-
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(JWKS_URL)
-
-#     if response.status_code != 200:
-#         raise Exception("Failed to fetch JWKS from Cognito")
-    
-#     jwks = response.json()
-#     for key_data in jwks["keys"]:
-#         if key_data["alg"] == "RS256" and key_data["use"] == "sig":
-#             key = jwk.construct(key_data)
-#             return key.to_pem().decode("utf-8")
-
-#     raise Exception("Failed to find a suitable public key in JWKS")
-
-        
-# async def get_token(request: Request):
-#     token = request.query_params.get("token")
-    
-#     if not token:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is required")
-#     return token
-
-# async def get_current_user(token: str = Depends(get_token)) -> str:
-#     JWKS_URL = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
-    
-#     client = PyJWKClient(JWKS_URL)
-    
-#     try:
-#         header = jwt.get_unverified_header(token)
-#         key = client.get_signing_key(header["kid"])
-#         public_key = key.key
-#         payload = jwt.decode(token, public_key, algorithms=["RS256"])
-#         return payload["sub"]
-#     except jwt.ExpiredSignatureError:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
-#     except jwt.InvalidSignatureError:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
-        
 
 app = FastAPI(
     title="Money Minder Api",
@@ -76,8 +26,6 @@ def create_transation(
     sub: str = Depends(get_current_user),
     token: str = ''
 ):
-    
-    # print('type', type(uuid.uuid4()))
     return tableClient.put_item(item={
         "Title": transaction.Title,
         "Category": transaction.Category,
@@ -130,13 +78,11 @@ def update_transaction(
 @app.post('/user/create', tags=['user'])
 def create_user(
     login_request: LoginRequest,
-    # token: str, 
-    # sub: str = Depends(get_current_user)
 ):
     tableCLientResponse = {}
     data_dict = {}
     createUserResponse = authClient.admin_create_user(login_request.email, login_request.password)
-    print('createUserResponse', createUserResponse)
+    
     if createUserResponse['status_code'] and createUserResponse['status_code'] >= 200 and createUserResponse['status_code'] < 204:
         tableClient.set_table(os.environ.get('user_transaction_name'))
         tableCLientResponse = tableClient.put_item(item={
@@ -146,8 +92,6 @@ def create_user(
         })
         json_content = tableCLientResponse.body.decode()
         data_dict = json.loads(json_content)
-    
-    # print('data_dict', data_dict)
 
     data = {
         'create-user-cognito': json.loads(json.dumps(createUserResponse, default=authClient.serialize_datetime)),
